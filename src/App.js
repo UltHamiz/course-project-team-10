758d3077b2f8532b98983b4ce85fd1d23e0aeb08
Abhinav Gaddipati <asg12@illinois.edu>
2023-03-14 21:50:25 -0500

    added React and Flask Architecture. Used get trend monthly income and display on page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import IncomeTrend from "./IncomeTrend";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Income Trend</h1>
+      </header>
+      <IncomeTrend />
+    </div>
+  );
+}
+
+export default App;
