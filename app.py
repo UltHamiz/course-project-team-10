bbacb14cf666d766b2bf599c7abe949e87e061dd
Abhisg5 <gaddipatia@gmail.com>
2023-04-21 13:36:40 -0500

    changed to income spending page

--- app.py
+++ app.py
@@ -9,56 +9,131 @@ from database import *
 app = Flask(__name__) 
 
 
-#begin testing
-transaction_name1 = "Grocery"
-amount1 = 100
-category1 = "Food"
-notes1 = "Shopped at Target"
-types1 = "Cash"
+spend = Spend("Groceries", 200, "Groceries", "", "Debit")
+spend2 = Spend("Rent", 600, "Rent", "", "Debit")
+spend3 = Spend("Party", 200, "Party", "", "Debit")
+spend4 = Spend("Groceries", 200, "Groceries", "", "Debit")
+spend5 = Spend("Rent", 600, "Rent", "", "Debit")
+spend6 = Spend("Party", 200, "Party", "", "Debit")
+spend7 = Spend("Groceries", 200, "Groceries", "", "Debit")
+spend8 = Spend("Rent", 600, "Rent", "", "Debit")
+spend9 = Spend("Party", 200, "Party", "", "Debit")
+spend10 = Spend("Groceries", 200, "Groceries", "", "Debit")
+spend11 = Spend("Rent", 600, "Rent", "", "Debit")
+spend12 = Spend("Party", 200, "Party", "", "Debit")
+spend13 = Spend("Groceries", 200, "Groceries", "", "Debit")
+spend14 = Spend("Rent", 600, "Rent", "", "Debit")
+spend15 = Spend("Party", 200, "Party", "", "Debit")
+spend16 = Spend("Groceries", 200, "Groceries", "", "Debit")
+spend17 = Spend("Rent", 600, "Rent", "", "Debit")
+spend18 = Spend("Party", 200, "Party", "", "Debit")
+spend19 = Spend("Groceries", 200, "Groceries", "", "Debit")
+spend20 = Spend("Rent", 600, "Rent", "", "Debit")
+spend21 = Spend("Party", 200, "Party", "", "Debit")
+spend22 = Spend("Groceries", 200, "Groceries", "", "Debit")
+spend23 = Spend("Rent", 600, "Rent", "", "Debit")
+spend24 = Spend("Party", 200, "Party", "", "Debit")
+spend25 = Spend("Groceries", 200, "Groceries", "", "Debit")
+spend26 = Spend("Rent", 600, "Rent", "", "Debit")
+spend27 = Spend("Party", 200, "Party", "", "Debit")
+spend28 = Spend("Groceries", 200, "Groceries", "", "Debit")
+spend29 = Spend("Rent", 600, "Rent", "", "Debit")
+spend30 = Spend("Party", 200, "Party", "", "Debit")
+spend31 = Spend("Groceries", 200, "Groceries", "", "Debit")
+spend32 = Spend("Rent", 600, "Rent", "", "Debit")
+spend33 = Spend("Party", 200, "Party", "", "Debit")
+spend34 = Spend("Groceries", 200, "Groceries", "", "Debit")
+spend35 = Spend("Rent", 600, "Rent", "", "Debit")
+spend36 = Spend("Party", 200, "Party", "", "Debit")
 
-transaction_name2 = "Salary"
-amount2 = 200
-category2 = "Work"
-notes2 = "CA for course"
-types2 = "Debit"
-
-transaction_name3 = "Gift for bday"
-amount3 = 500
-category3 = "Gift"
-notes3 = "Thanks Hamiz"
-types3 = "Cash"
-
-transaction_name4 = "Too much"
-amount4 = 500
-category4 = "Gift"
-notes4 = "Return money to Hamiz"
-types4 = "Checking"
-
-
-#Spending/Income Transaction Objects
-spend = Spend(transaction_name1, amount1, category1, notes1, types1)
-income = Income(transaction_name2, amount2, category2, notes2, types2)
-income2 = Income(transaction_name3, amount3, category3, notes3, types3)
-spend2 = Spend(transaction_name4, amount4, category4, notes4, types4)
-
-spend3 = Spend(transaction_name4, amount1, category1, notes1, types1)
-person = Person("Jacob", {}, {}, 1000)
-
-person.add_income(income, datetime.date(2022, 12, 1))
-person.add_income(income2, datetime.date(2022, 12, 3))
-person.add_income(income2, datetime.date(2022, 7, 5))
-person.add_income(income2, datetime.date(2022, 6, 14))
-person.add_income(income2, datetime.date(2022, 5, 31))
-person.add_income(income2, datetime.date(2022, 4, 22))
-person.add_income(income2, datetime.date(2022, 2, 3))
-person.add_income(income2, datetime.date(2022, 3, 30))
+spend37 = Spend("Car Damanges", 400, "Car", "", "Debit")
+spend38 = Spend("Ticket", 200, "Ticket", "", "Debit")
+spend39 = Spend("Doctor", 500, "Health", "", "Debit")
 
 
-person.add_spending(spend, datetime.date(2022, 12, 5))
-person.add_spending(spend2, datetime.date(2022, 12, 15))
-person.add_spending(spend3, datetime.date(2023, 1, 2))
+person = Person("Jacob", {}, {}, 1200)
+
+income = Income("Work", 1400, "Work", "", "Debit")
+income2 = Income("Work", 1400, "Work", "", "Debit")
+income3 = Income("Work", 1400, "Work", "", "Debit")
+income4 = Income("Work", 1400, "Work", "", "Debit")
+income5 = Income("Work", 1400, "Work", "", "Debit")
+income6 = Income("Work", 1400, "Work", "", "Debit")
+income7 = Income("Work", 1400, "Work", "", "Debit")
+income8 = Income("Work", 1400, "Work", "", "Debit")
+income9 = Income("Work", 1400, "Work", "", "Debit")
+income10 = Income("Work", 1400, "Work", "", "Debit")
+income11 = Income("Work", 1400, "Work", "", "Debit")
+income12 = Income("Work", 1400, "Work", "", "Debit")
+
+
+income17 = Income("Birthday", 200, "Gift", "", "Cash")
+income18 = Income("Raffle", 20, "Gift", "", "Venmo")
+income19 = Income("Fantasy Football", 50, "Gift", "", "Venmo")
+income20 = Income("March Madness", 75, "Gift", "", "Venmo")
+income21 = Income("Christmas", 100, "Gift", "", "Cash")
+
+
+
+person.add_income(income, datetime.date(2022, 1, 1))
+person.add_income(income2, datetime.date(2022, 2, 1))
+person.add_income(income3, datetime.date(2022, 3, 1))
+person.add_income(income4, datetime.date(2022, 4, 3))
+person.add_income(income5, datetime.date(2022, 5, 1))
+person.add_income(income6, datetime.date(2022, 6, 3))
+person.add_income(income7, datetime.date(2022, 7, 1))
+person.add_income(income8, datetime.date(2022, 8, 3))
+person.add_income(income9, datetime.date(2022, 9, 1))
+person.add_income(income10, datetime.date(2022, 10, 3))
+person.add_income(income11, datetime.date(2022, 11, 1))
+person.add_income(income12, datetime.date(2022, 12, 3))
+person.add_income(income17, datetime.date(2022, 6, 20))
+person.add_income(income18, datetime.date(2022, 8, 15))
+person.add_income(income19, datetime.date(2022, 3, 15))
+person.add_income(income20, datetime.date(2022, 3, 7))
+person.add_income(income21, datetime.date(2022, 12, 25))
+
+
+
+
+person.add_spending(spend, datetime.date(2022, 1, 10))
 person.add_spending(spend2, datetime.date(2022, 1, 15))
-person.add_spending(spend3, datetime.date(2023, 1, 1))
+person.add_spending(spend3, datetime.date(2022, 1, 17))
+person.add_spending(spend4, datetime.date(2022, 2, 20))
+person.add_spending(spend5, datetime.date(2022, 2, 15))
+person.add_spending(spend6, datetime.date(2022, 2, 17))
+person.add_spending(spend7, datetime.date(2022, 3, 10))
+person.add_spending(spend8, datetime.date(2022, 3, 15))
+person.add_spending(spend9, datetime.date(2022, 3, 17))
+person.add_spending(spend10, datetime.date(2022, 4, 10))
+person.add_spending(spend11, datetime.date(2022, 4, 15))
+person.add_spending(spend12, datetime.date(2022, 4, 17))
+person.add_spending(spend13, datetime.date(2022, 5, 10))
+person.add_spending(spend14, datetime.date(2022, 5, 15))
+person.add_spending(spend15, datetime.date(2022, 5, 17))
+person.add_spending(spend16, datetime.date(2022, 6, 10))
+person.add_spending(spend17, datetime.date(2022, 6, 15))
+person.add_spending(spend18, datetime.date(2022, 6, 17))
+person.add_spending(spend19, datetime.date(2022, 7, 10))
+person.add_spending(spend20, datetime.date(2022, 7, 15))
+person.add_spending(spend21, datetime.date(2022, 7, 17))
+person.add_spending(spend22, datetime.date(2022, 8, 10))
+person.add_spending(spend23, datetime.date(2022, 8, 15))
+person.add_spending(spend24, datetime.date(2022, 8, 17))
+person.add_spending(spend25, datetime.date(2022, 9, 10))
+person.add_spending(spend26, datetime.date(2022, 9, 15))
+person.add_spending(spend27, datetime.date(2022, 9, 17))
+person.add_spending(spend28, datetime.date(2022, 10, 10))
+person.add_spending(spend29, datetime.date(2022, 10, 15))
+person.add_spending(spend30, datetime.date(2022, 10, 17))
+person.add_spending(spend31, datetime.date(2022, 11, 10))
+person.add_spending(spend32, datetime.date(2022, 11, 15))
+person.add_spending(spend33, datetime.date(2022, 11, 17))
+person.add_spending(spend34, datetime.date(2022, 12, 10))
+person.add_spending(spend35, datetime.date(2022, 12, 15))
+person.add_spending(spend36, datetime.date(2022, 12, 17))
+person.add_spending(spend37, datetime.date(2022, 10, 27))
+person.add_spending(spend38, datetime.date(2022, 2, 9))
 #end of testing
 
 # index.html routes
@@ -103,7 +178,11 @@ def add_spending():
 @app.route('/api/income/<int:year>', methods=['GET'])
 def get_yearly_income_by_cat(year):
     yearly_income = person.get_yearly_income_by_category(year)
-    print(yearly_income)
+    return jsonify(yearly_income)
+
+@app.route('/api/spend/<int:year>', methods=['GET'])
+def get_yearly_spend_by_cat(year):
+    yearly_income = person.get_yearly_spend_by_category(year)
     return jsonify(yearly_income)
 
 # graph.html routes
@@ -125,6 +204,12 @@ def trend_monthly_spend(year, month):
     trend_data = person.get_trend_monthly_spend(month, year)
     return jsonify(trend_data.to_dict(orient='records'))
 
+@app.route('/trend_yearly_spend/<int:year>', methods=['GET'])
+def trend_yearly_spend(year):
+    trend_data = person.get_trend_yearly_spend(year)
+    return jsonify(trend_data.to_dict(orient='records'))
+
+
 
 @app.route('/trend_monthly_diff/<int:year>/<int:month>', methods=['GET'])
 def trend_monthly_difference(year, month):
@@ -136,10 +221,17 @@ def trend_monthly_difference(year, month):
 def graph():
     return render_template('graph.html')
 
-@app.route('/cat', methods=['POST'])
+@app.route('/cat', methods=['GET'])
 def cat():
     return render_template('cat.html')
 
 
+@app.route('/income', methods=['GET'])
+def incomePage():
+    return render_template('income.html')
+
+@app.route('/spending', methods=['GET'])
+def spendingPage():
+    return render_template('spending.html')
 if __name__ == '__main__':
     app.run(debug=True)
